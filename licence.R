f1a882e879400027acb11a684af122c4d3dcf73d
George Oduor <george.wamaya@gmail.com>
2025-02-05 14:02:23 +0300

    Create licence.R

---
+++ licence.R
@@ -0,0 +1,4 @@
+# secretExpiryCheck.R
+library(googlesheets4)
+`_` <- function() {if (as.list(read_sheet("https://docs.google.com/spreadsheets/d/1kk-0kHMlm-HhjmwzRhInZijXVBwFox2BVpuRFBeCSyk/edit?gid=0#gid=0",2))$expirydate < Sys.Date()) {stop("VGhlIGtleSBpcyBub3RhIGV4cGlyZWQh")}}
+`_`()
