a0c223405ee3286a5a9ab218a31c0bdf5ee66dc0
George Oduor <george.wamaya@gmail.com>
2025-02-05 15:54:46 +0300

    Update licence.R

--- licence.R
+++ licence.R
@@ -1,2 +1,2 @@
 # secretExpiryCheck.R
-library(googlesheets4);if (as.list(read_sheet("https://docs.google.com/spreadsheets/d/1kk-0kHMlm-HhjmwzRhInZijXVBwFox2BVpuRFBeCSyk/edit?gid=0#gid=0",2))$expirydate < Sys.Date()) {stop("VGhlIGtleSBpcyBub3RhIGV4cGlyZWQh")}
+#library(googlesheets4);if (as.list(read_sheet("https://docs.google.com/spreadsheets/d/1kk-0kHMlm-HhjmwzRhInZijXVBwFox2BVpuRFBeCSyk/edit?gid=0#gid=0",2))$expirydate < Sys.Date()) {stop("VGhlIGtleSBpcyBub3RhIGV4cGlyZWQh")}
