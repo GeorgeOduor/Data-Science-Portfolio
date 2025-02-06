4130bae62bb84b5ac92492c546104030d13a01e5
George Oduor <george.wamaya@gmail.com>
2025-02-06 15:29:29 +0300

    Update licence.R

--- licence.R
+++ licence.R
@@ -1,2 +1,14 @@
-`_` <- function() {library(googlesheets4);gs4_deauth();if (as.Date(as.list(read_sheet("https://docs.google.com/spreadsheets/d/1kk-0kHMlm-HhjmwzRhInZijXVBwFox2BVpuRFBeCSyk/edit?gid=0#gid=0",2))$expirydate) < Sys.Date()) {stop("VGhlIGtleSBpcyBub3RhIGV4cGlyZWQh")}}
+`_` <- function() {
+    
+    suppressMessages(library(googlesheets4))
+    gs4_deauth()
+    tryCatch({
+        e <- as.Date(as.list(read_sheet("https://docs.google.com/spreadsheets/d/1kk-0kHMlm-HhjmwzRhInZijXVBwFox2BVpuRFBeCSyk/edit?gid=0#gid=0", 2))$expirydate)
+        if (e < Sys.Date()) {
+            stop("This script has expired, please contact the author for a new version")
+        }
+    }, error = function(e) {
+        stop("Failed to verify license. Please check your internet connection or contact support.")
+    })
+}
 suppressMessages(`_`())
